40f66c57e0261e393a8644968e42c2a3a696f6f6
d_dudin <dudindmitry02@gmail.com>
2026-04-07 22:59:20 +0300

    [DP4-013] Small base exception fix

--- Backend/Utils/Exceptions/BaseFormattedException.h
+++ Backend/Utils/Exceptions/BaseFormattedException.h
@@ -15,9 +15,13 @@ class BaseFormattedException : public std::exception
 public:
 	explicit BaseFormattedException(const char* InFile, const char* InFunctionName,const int InCodeLineNumber, const char* InMessage = nullptr)
 	{
-		const char* file = InFile ? InFile : "UnknownFile";
-		const char* function = InFunctionName ? InFunctionName : "UnknownFunction";
-		const char* message = InMessage ? InMessage : "No message was provided for this exception";
+		constexpr std::string_view defaultMessage = "No message was provided for this exception";
+		constexpr std::string_view defaultFile = "UnknownFile";
+		constexpr std::string_view defaultFunction = "UnknownFunction";
+
+		const std::string_view file = InFile ? InFile : defaultFile;
+		const std::string_view function = InFunctionName ? InFunctionName : defaultFunction;
+		const std::string_view message = InMessage ? InMessage : defaultMessage;
 
 		std::ostringstream oss;
 		oss << "Exception at ["
